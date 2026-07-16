bfbad5757b8d7eb58fd7a1e93624e69810146fde
RAEEZ9778 <raeeezsafa9778@gmail.com>
2026-07-16 08:54:12 +0530

    Updated Patient Registration Module

--- appointment.py
+++ appointment.py
@@ -1,1 +1,12 @@
-print("Hospital Appointment Booking System!")
+print("Hospital Appointment Booking System!")\
+
+def reg_patient():
+    print("Registering a new patient...")
+    print("Please enter patient details:")
+    print("Name: ")
+    name = input()
+    print("Age: ")
+    age = int(input())
+    print("Contact Number: ")
+    contact = input()
+    print("Patient registered successfully!")
